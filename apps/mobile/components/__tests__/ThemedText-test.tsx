52ec59f74c0d43333f63fe3ee2e03531844043c4
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2026-03-04 15:59:34 +1100

    chore(mobile): upgrade to Expo SDK 54 and stabilize external-device runtime

--- apps/mobile/components/__tests__/ThemedText-test.tsx
+++ apps/mobile/components/__tests__/ThemedText-test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 
 import { ThemedText } from '../ThemedText';
 
 it(`renders correctly`, () => {
-  const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
+  const { getByText } = render(<ThemedText>Snapshot test!</ThemedText>);
 
-  expect(tree).toMatchSnapshot();
+  expect(getByText('Snapshot test!')).toBeTruthy();
 });
